23ecf42a77dbbf7a5beffdcc9a2407f150822b85
Rob van der Most <silvester747@gmail.com>
2020-05-02 19:12:06 +0200

    Fix linter issue

--- asciidoxy/__init__.py
+++ asciidoxy/__init__.py
@@ -15,3 +15,8 @@
 
 from ._version import (__title__, __description__, __url__, __version__, __author__,
                        __author_email__, __license__)
+
+__all__ = [
+    "__title__", "__description__", "__url__", "__version__", "__author__", "__author_email__",
+    "__license__"
+]
