8cd2d06f3ac632c4481bb4537f235ca9600d7518
Rob van der Most <silvester747@gmail.com>
2020-05-21 11:18:10 +0200

    Tokenizer for types from text

--- asciidoxy/doxygenparser/type_parser.py
+++ asciidoxy/doxygenparser/type_parser.py
@@ -13,9 +13,12 @@
 # limitations under the License.
 """Parsing of types from strings and XML."""
 
+import string
+
 import xml.etree.ElementTree as ET
 
-from typing import Optional, Type, Union
+from enum import Enum, auto
+from typing import List, Optional, Tuple, Type, Union
 
 from .driver_base import DriverBase
 from .language_traits import LanguageTraits
@@ -86,3 +89,193 @@ def parse_type(traits: Type[LanguageTraits],
 
     type_ref, _ = extract_type(type_element.iter("ref"), type_element.text)
     return type_ref
+
+
+class TokenType(Enum):
+    UNKNOWN = auto()
+    WHITESPACE = auto()
+    QUALIFIER = auto()
+    OPERATOR = auto()
+    NAME = auto()
+    NESTED_START = auto()
+    NESTED_END = auto()
+    NESTED_SEPARATOR = auto()
+
+
+class Token:
+    type_: TokenType
+
+    def __init__(self, type_: TokenType = TokenType.UNKNOWN):
+        self.type_ = type_
+
+    def __eq__(self, other) -> bool:
+        return self.type_ == other.type_
+
+
+class TextToken(Token):
+    text: str
+
+    def __init__(self, text: str, type_: TokenType = TokenType.UNKNOWN):
+        super().__init__(type_)
+        self.text = text
+
+    def __eq__(self, other) -> bool:
+        return super().__eq__(other) and self.text == other.text
+
+    def __str__(self) -> str:
+        return f"{self.type_}: {repr(self.text)}"
+
+    __repr__ = __str__
+
+
+class TypeParseError(Exception):
+    msg: str
+
+    def __init__(self, msg: str):
+        self.msg = msg
+
+    def __str__(self) -> str:
+        return f"Failed to parse type: {self.msg}"
+
+
+class Tokenizer:
+    NESTED_STARTS = "<", "[",
+    NESTED_ENDS = ">", "]",
+    NESTED_SEPARATORS = ",", ";",
+    OPERATORS = "*", "&",
+    BOUNDARIES = (NESTED_STARTS + NESTED_ENDS + NESTED_SEPARATORS + OPERATORS +
+                  tuple(string.whitespace))
+
+    QUALIFIERS = "const", "volatile", "constexpr", "mutable", "enum", "class",
+
+    @classmethod
+    def tokenize_text(cls, text: str) -> List[TextToken]:
+        tokens: List[TextToken] = []
+
+        def append_token(text: str) -> None:
+            # TODO Maybe simplify and do not care about multiple white space tokens?
+            token = cls.make_text_token(text)
+            if token.type_ == TokenType.WHITESPACE and tokens and tokens[-1].type_ == token.type_:
+                tokens[-1].text += text
+            else:
+                tokens.append(token)
+
+        while text:
+            for i, c in enumerate(text):
+                if c in cls.BOUNDARIES:
+                    if i > 0:
+                        append_token(text[:i])
+                    append_token(text[i])
+                    text = text[i + 1:]
+                    break
+            else:
+                append_token(text)
+                text = ""
+
+        return tokens
+
+    @classmethod
+    def make_text_token(cls, text: str) -> TextToken:
+        if text.isspace():
+            type_ = TokenType.WHITESPACE
+        elif text in cls.NESTED_STARTS:
+            type_ = TokenType.NESTED_START
+        elif text in cls.NESTED_ENDS:
+            type_ = TokenType.NESTED_END
+        elif text in cls.NESTED_SEPARATORS:
+            type_ = TokenType.NESTED_SEPARATOR
+        elif text in cls.OPERATORS:
+            type_ = TokenType.OPERATOR
+        elif text in cls.QUALIFIERS:
+            type_ = TokenType.QUALIFIER
+        else:
+            type_ = TokenType.NAME
+
+        return TextToken(text, type_)
+
+
+class TypeProducer:
+    ALLOWED_PREFIXES = TokenType.WHITESPACE, TokenType.OPERATOR, TokenType.QUALIFIER,
+    ALLOWED_SUFFIXES = TokenType.WHITESPACE, TokenType.OPERATOR, TokenType.QUALIFIER,
+    ALLOWED_NAMES = TokenType.WHITESPACE, TokenType.NAME,
+
+    @classmethod
+    def type_from_tokens(cls, tokens: List[TextToken]) -> TypeRef:
+        original_tokens = tokens
+        tokens = tokens[:]
+
+        prefixes, tokens = cls.select_tokens(tokens, cls.ALLOWED_PREFIXES)
+        names, tokens = cls.select_tokens(tokens, cls.ALLOWED_NAMES)
+        tokens[:0] = cls.remove_trailing_whitespace(names)
+
+        if not names:
+            raise TypeParseError(f"No name found"
+                                 f" in `{''.join(t.text for t in original_tokens)}`")
+
+        nested_types, tokens = cls.nested_types(tokens)
+        suffixes, tokens = cls.select_tokens(tokens, cls.ALLOWED_SUFFIXES)
+
+        if tokens:
+            raise TypeParseError(f"Unexpected characters `{''.join(t.text for t in tokens)}`"
+                                 f" in `{''.join(t.text for t in original_tokens)}`")
+
+        type_ref = TypeRef("lang")
+        type_ref.name = "".join(n.text for n in names)
+        type_ref.prefix = "".join(p.text for p in prefixes)
+        type_ref.suffix = "".join(s.text for s in suffixes)
+        type_ref.nested = nested_types or []
+        return type_ref
+
+    @staticmethod
+    def select_tokens(tokens, types) -> Tuple[List[TextToken], List[TextToken]]:
+        for i, t in enumerate(tokens):
+            if t.type_ not in types:
+                return tokens[:i], tokens[i:]
+        return tokens, []
+
+    @staticmethod
+    def remove_trailing_whitespace(tokens) -> List[TextToken]:
+        ret = []
+        while tokens and tokens[-1].type_ == TokenType.WHITESPACE:
+            ret.append(tokens.pop(-1))
+        return ret
+
+    @classmethod
+    def nested_types(cls,
+                     tokens: List[TextToken]) -> Tuple[Optional[List[TypeRef]], List[TextToken]]:
+        original_tokens = tokens
+        tokens = tokens[:]
+        nested_types = []
+
+        for i, t in enumerate(tokens):
+            if t.type_ == TokenType.WHITESPACE:
+                continue
+            elif t.type_ == TokenType.NESTED_START:
+                tokens = tokens[i + 1:]
+                break
+            else:
+                return None, tokens
+        else:
+            return None, tokens
+
+        level = 0
+        while tokens:
+            for i, t in enumerate(tokens):
+                if t.type_ == TokenType.NESTED_START:
+                    level += 1
+                elif level > 0 and t.type_ == TokenType.NESTED_END:
+                    level -= 1
+                elif level == 0 and t.type_ in (TokenType.NESTED_SEPARATOR, TokenType.NESTED_END):
+                    nested_type = cls.type_from_tokens(tokens[:i])
+                    nested_types.append(nested_type)
+                    tokens = tokens[i + 1:]
+
+                    if t.type_ == TokenType.NESTED_END:
+                        return nested_types, tokens
+                    else:
+                        break
+            else:
+                break
+
+        raise TypeParseError("Unexpected end of nested types:"
+                             f" `{''.join(t.text for t in original_tokens)}`")
